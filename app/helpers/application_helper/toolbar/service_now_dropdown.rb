a43ba9e0d9c3c2878b611af087cad4ecfda3e765
eilam <eilam.elimelech@kyndryl.com>
2023-02-13 11:54:11 +0200

    update dropdown

--- app/helpers/application_helper/toolbar/service_now_dropdown.rb
+++ app/helpers/application_helper/toolbar/service_now_dropdown.rb
@@ -1,4 +1,22 @@
 class ApplicationHelper::Toolbar::ServiceNowDropdown < ApplicationHelper::Toolbar::Basic
+  auth = Authentication.in_my_region.find_by(:name => "service now")
+  if auth.blank?
+    button_group('service_now', [
+      select(
+        :service_now_choice,
+        nil,
+        t = N_('Service Now'),
+        t,
+        :items => [
+          button(
+            :service_now_connect,
+            'fa fa-refresh fa-lg',
+            t = N_('Connect'),
+            t)
+        ]
+      ),
+    ])
+  else
     button_group('service_now', [
       select(
         :service_now_choice,
@@ -9,10 +27,11 @@ class ApplicationHelper::Toolbar::ServiceNowDropdown < ApplicationHelper::Toolba
           button(
             :service_now_new,
             'fa fa-refresh fa-lg',
-            N_('Create a ticket'),
-            N_('Create a ticket'))
+            t = N_('Create a ticket'),
+            t)
         ]
       ),
     ])
+  end
 end
 
