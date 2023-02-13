9db8457a0ba30c759a7fa7a8083db1e684d14150
eilam <eilam.elimelech@kyndryl.com>
2023-02-13 11:53:00 +0200

    add fields

--- app/javascript/components/service-now-form/service-now-form.schema.js
+++ app/javascript/components/service-now-form/service-now-form.schema.js
@@ -3,24 +3,6 @@ import { componentTypes, validatorTypes } from '@@ddf';
 const createSchema = () => ({
   fields: [{
     component: componentTypes.TEXT_FIELD,
-    name: 'domain',
-    label: 'Service Now domain',
-    isRequired: true,
-    validate: [{ type: validatorTypes.REQUIRED }]
-  },{
-    component: componentTypes.TEXT_FIELD,
-    name: 'table',
-    label: 'Table name',
-    isRequired: true,
-    validate: [{ type: validatorTypes.REQUIRED }]
-  },{
-    component: componentTypes.TEXT_FIELD,
-    name: 'username',
-    label: 'User name',
-    isRequired: true,
-    validate: [{ type: validatorTypes.REQUIRED }]
-  },{
-    component: componentTypes.TEXT_FIELD,
     name: 'subject',
     label: 'Subject',
     isRequired: true,
@@ -36,3 +18,4 @@ const createSchema = () => ({
 
 
 export default createSchema;
+
