417f45a656247ffccfd3bd6d17019191c8c5ad8a
eilam <eilam.elimelech@kyndryl.com>
2023-02-13 11:53:43 +0200

    add connect

--- app/controllers/service_now_controller.rb
+++ app/controllers/service_now_controller.rb
@@ -6,12 +6,18 @@ class ServiceNowController < ApplicationController
   include Mixins::GenericFormMixin
   include Mixins::GenericButtonMixin
 
-
   def new
     assert_privileges("service_now_new")
     @in_a_form = true
-    drop_breadcrumb(:name => _("Add New Service Now "), :url => "/service_now/new")
     @layout = "service_now"
+    drop_breadcrumb(:name => _("Add New Service Now Ticket"), :url => "/service_now/new")
+  end
+
+  def connect
+    assert_privileges("service_now_connect")
+    @in_a_form = true
+    @layout = "service_now"
+    drop_breadcrumb(:name => _("Add New Service Now Instance "), :url => "/service_now/connect")
   end
 
 
